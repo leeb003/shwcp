5d98b35cee4dd04e0a6d632a3be83a1fbc7d4964
root <root@localhost.localdomain>
2019-11-27 14:02:46 -0700

    Added translation setup for gutenberg js string tranlsation, might need to do more for actual translation

--- includes/class-wcp-gutenberg.php
+++ includes/class-wcp-gutenberg.php
@@ -3,21 +3,28 @@
  * Gutenberg functionality
  */
 
-//include_once('src/metabox.php');
 function myprefix_enqueue_assets() {
 	$main_wcp = new main_wcp;
 	$location = 'gutenberg'; // where we are using so we can format correctly
 	$wcp_databases = $main_wcp->wcp_getdbs($location);
     wp_enqueue_script(
-        'myprefix-gutenberg-sidebar', SHWCP_ROOT_URL . '/assets/js/guten-build/index.js',
+        'shwcp-pagedb-script', SHWCP_ROOT_URL . '/assets/js/guten-build/index.js',
         //plugins_url( 'build/index.js', __FILE__ ),
         array( 'wp-blocks','wp-plugins', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-components', 'wp-data', 'wp-compose', 'wp-api-fetch' )
     );
 	// get an array of current databases to pass to database selection on page from main_wcp
-	wp_localize_script('myprefix-gutenberg-sidebar', 'wcp_databases', $wcp_databases);
+	wp_localize_script('shwcp-pagedb-script', 'wcp_databases', $wcp_databases);
 	//print_r($wcp_databases);
 }
 add_action( 'enqueue_block_editor_assets', 'myprefix_enqueue_assets' );
+
+/**
+ * Add Gutenberg js translations to WP
+ */
+function shwcp_pagedb_translations() {
+	    wp_set_script_translations( 'shwcp-pagedb-script', 'shwcp' );
+}
+add_action( 'init', 'shwcp_pagedb_translations' );
 
 /**
  * Register DB Select (wcp_db_select) Meta Field to Rest API
